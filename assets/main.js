6d5bafd674b79d70868b5d065855456785742566
udanielnogueira <danielsilvangr@gmail.com>
2023-01-18 12:40:11 -0300

    Update file

--- assets/main.js
+++ assets/main.js
@@ -2,29 +2,35 @@ var imgAshe = '<img class="imgCampeao" src="./assets/ashe.png">';
 var imgJinx = '<img class="imgCampeao" src="./assets/jinx.png">';
 var imgAhri = '<img class="imgCampeao" src="./assets/ahri.png">';
 var imgLux = '<img class="imgCampeao" src="./assets/lux.png">';
+var imgZed = '<img class="imgCampeao" src="./assets/zed.png">';
 
-function exibirAshe(){
+function exibir(imagem) {
     eFrame = document.getElementById("frame");
-    eFrame.innerHTML = imgAshe;
-}
-
-function exibirJinx(){
-    eFrame = document.getElementById("frame");
-    eFrame.innerHTML = imgJinx;
-}
-
-function exibirAhri(){
-    eFrame = document.getElementById("frame");
-    eFrame.innerHTML = imgAhri;
-}
-
-function exibirLux(){
-    eFrame = document.getElementById("frame");
-    eFrame.innerHTML = imgLux;
+    
+    if (imagem == 1)
+    {
+        eFrame.innerHTML = imgAshe;
+    }
+    else if (imagem == 2)
+    {
+        eFrame.innerHTML = imgJinx;
+    }
+    else if (imagem == 3)
+    {
+        eFrame.innerHTML = imgAhri;
+    }
+    else if (imagem == 4)
+    {
+        eFrame.innerHTML = imgLux;
+    }
+    else if (imagem == 5)
+    {
+        eFrame.innerHTML = imgZed;
+    }
 }
 
 var audio = new Audio('./assets/ui-click.mp3');
 
-function playUIClick(){
+function playUIClick() {
     audio.play();
 }
