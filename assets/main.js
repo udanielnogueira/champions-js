62bc051e239595c68cefe624c70a4116dc824ca8
udanielnogueira <danielsilvangr@gmail.com>
2023-01-18 10:39:34 -0300

    Update file

---
+++ assets/main.js
@@ -0,0 +1,30 @@
+var imgAshe = '<img class="imgCampeao" src="./assets/ashe.png">';
+var imgJinx = '<img class="imgCampeao" src="./assets/jinx.png">';
+var imgAhri = '<img class="imgCampeao" src="./assets/ahri.png">';
+var imgLux = '<img class="imgCampeao" src="./assets/lux.png">';
+
+function exibirAshe(){
+    eFrame = document.getElementById("frame");
+    eFrame.innerHTML = imgAshe;
+}
+
+function exibirJinx(){
+    eFrame = document.getElementById("frame");
+    eFrame.innerHTML = imgJinx;
+}
+
+function exibirAhri(){
+    eFrame = document.getElementById("frame");
+    eFrame.innerHTML = imgAhri;
+}
+
+function exibirLux(){
+    eFrame = document.getElementById("frame");
+    eFrame.innerHTML = imgLux;
+}
+
+var audio = new Audio('./assets/soundtrack.mp3');
+
+function trilhaSonora(){
+    audio.play();
+}
